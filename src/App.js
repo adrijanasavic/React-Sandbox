ed47f3ecc3a4921e417e19449d5ff7aae0386553
Adrijana <adrijana.jovicic@gmail.com>
2022-02-09 17:20:10 +0100

    Custom Hook 2 - useLocalStorage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
+// import CustomHookExample1 from './components/CustomHookExample1';
 // import UseRefExample1 from './components/UseRefExample1';
 // import UseRefExample2 from './components/UseRefExample2';
 // import UseRefExample3 from './components/UseRefExample3';
 // import UseMemoExample from './components/UseMemoExample';
 // import UseCallbackExample from './components/UseCallbackExample';
-import CustomHookExample1 from './components/CustomHookExample1';
+// import CustomHookExample1 from './components/CustomHookExample1';
+import CustomHookExample2 from './components/CustomHookExample2';
+
 function App() {
   return (
     <div className="container mt-5">
-      <CustomHookExample1/>
+      <CustomHookExample2/>
     </div>
   );
 }
